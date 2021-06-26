ea89e7343b9dad6db3c5fbb996c53ac82a8982f6
hahazexia <zhouzx@suirui.com>
2021-06-26 18:08:54 +0800

    leetcode

--- leetcode/js/linkedList/_21_merge-two-sorted-lists.js
+++ leetcode/js/linkedList/_21_merge-two-sorted-lists.js
@@ -13,7 +13,42 @@
  */
  var mergeTwoLists = function(l1, l2) {
     if (!l1 || !l2) return l1 ? l1 : l2;
-
     
+    let res, c1, c2;
+
+    if (l1.val < l2.val) {
+        res = l1;
+        c1 = l1.next;
+        c2 = l2;
+    } else {
+        res = l2;
+        c1 = l1;
+        c2 = l2.next;
+    }
+    let c = res;
+
+    while(c1 || c2) {
+        if (!c1) {
+            c.next = c2;
+            c2 = null;
+            continue;
+        }
+        if (!c2) {
+            c.next = c1;
+            c1 = null;
+            continue;
+        }
+        if (c1.val < c2.val) {
+            c.next = c1;
+            c = c1;
+            c1 = c1.next;
+        } else {
+            c.next = c2;
+            c = c2;
+            c2 = c2.next;
+        }
+    }
+
+    return res;
 };
 
