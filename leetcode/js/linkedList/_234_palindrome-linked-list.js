418045e2b4a53bcd91cda2e5ec130b88778ed345
hahazexia <zhouzx@suirui.com>
2021-06-29 20:20:07 +0800

    leetcode

--- leetcode/js/linkedList/_234_palindrome-linked-list.js
+++ leetcode/js/linkedList/_234_palindrome-linked-list.js
@@ -24,3 +24,17 @@ var isPalindrome = function(head) {
 };
 
 
+var isPalindrome = function(head) {
+    let arr = [];
+
+    while(head) {
+        arr.push(head.val);
+        head = head.next;
+    }
+
+    for (let i = 0, j = arr.length - 1; i < j; i++, j--) {
+        if (arr[i] !== arr[j]) return false
+    }
+
+    return true;
+};
